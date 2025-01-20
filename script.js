ebecfa8e18d68d5855e702bacab02c503dbf2756
Aishwary Dixit <mr.dixit17@gmail.com>
2025-01-20 14:35:56 +0530

    Added about me & project section

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-const heading = "I turn Coffee into Code.";
+const heading = "Software Developer";
 
 const headingPara = document.querySelector("#heading-para");
+const projectTechStacks = document.querySelector(".project-techstacks");
 
 function pause(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function typeHeading() {
+  await pause(700);
   for (let i = 0; i < heading.length; i++) {
+    if(i==0) headingPara.textContent ="";
     headingPara.textContent += heading[i];
     await pause(100);
   }
 }
 typeHeading();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const animateElements = document.querySelectorAll(".animate");
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add("visible");
+              observer.unobserve(entry.target); 
+          }
+      });
+  }, { threshold: 0.2 }); 
+
+  animateElements.forEach(element => observer.observe(element));
+});
+
+const tech = "Kotlin, Jetpack Compose, MongoDB"
+const techstacksArray = tech.split(", ");
+techstacksArray.forEach( (skill) => {
+    let skillDiv = document.createElement("div");
+    skillDiv.classList.add("techstacks");
+    skillDiv.textContent = skill; 
+    projectTechStacks.appendChild(skillDiv);
+});
+
+
